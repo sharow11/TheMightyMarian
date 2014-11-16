e133985e8b25d680cfefc5d0ed9e8711f1ed9476
ciasteczko-i-mleczko <ciasteczko.i.mleczko@gmail.com>
2014-11-16 21:07:21 +0100

    gravity falls?
    jest boss, na razie gowniany, ale zawsze. lvl 0 jest lvlem z bossem,
    wszystko sklada sie z jednego pokoju.
    drabina jest wstepnie odblokowana bo nei umiem jeszcze sprawic zeby na
    smierc wroga sie odblokowywala, powinno dzialac.

--- TheMightyMarian/Assets/Scripts/Ladder.cs
+++ TheMightyMarian/Assets/Scripts/Ladder.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Ladder : MonoBehaviour {
 
+    public bool disabled = false;
 	// Use this for initialization
 	void Start () {
 	}
@@ -14,6 +15,8 @@ public class Ladder : MonoBehaviour {
 
     void OnTriggerEnter(Collider other)
     {
+        if (disabled)
+            return;
         if (other.gameObject.layer == LayerMask.NameToLayer("Marian"))
         {
             GameManager gm = (GameManager)FindObjectOfType(typeof(GameManager));
