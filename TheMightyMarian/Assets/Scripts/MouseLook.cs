965c60f34d40f1565dcf386f43ae875811056934
Tobiasz931 <tobiasz931@gmail.com>
2014-12-02 01:36:52 +0100

    Mini game

---
+++ TheMightyMarian/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,90 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/// MouseLook rotates the transform based on the mouse delta.
+/// Minimum and Maximum values can be used to constrain the possible rotation
+
+/// To make an FPS style character:
+/// - Create a capsule.
+/// - Add the MouseLook script to the capsule.
+///   -> Set the mouse look to use LookX. (You want to only turn character but not tilt it)
+/// - Add FPSInputController script to the capsule
+///   -> A CharacterMotor and a CharacterController component will be automatically added.
+
+/// - Create a camera. Make the camera a child of the capsule. Reset it's transform.
+/// - Add a MouseLook script to the camera.
+///   -> Set the mouse look to use LookY. (You want the camera to tilt up and down like a head. The character already turns.)
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour
+{
+    GameObject marian;
+    public GameObject rocket;
+	public float sensitivityX = 5f;
+	public float sensitivityY = 5f;
+
+	public float minimumX = -360f;
+	public float maximumX = 360f;
+
+	public float minimumY = -90f;
+	public float maximumY = 90f;
+    GameObject head;
+	float rotationY = 0F;
+    public float projSpeed = 20f;
+    public float range = 1.5f;
+    public float pushMultiplayer = 10f;
+    List<float> rTimes = new List<float>();
+    List<Vector3> rPoints = new List<Vector3>();
+
+    void Update()
+    {
+        while (rTimes.Count!=0 && rTimes[0] < Time.time)
+        {
+            float dist = Vector3.Distance(marian.transform.position, rPoints[0]);
+            Debug.Log("Boom " + dist);
+            if (dist < range)
+            {
+                marian.rigidbody.velocity = marian.rigidbody.velocity + (marian.transform.position - rPoints[0]).normalized * (range - dist) * pushMultiplayer;
+            }
+            rTimes.RemoveAt(0);
+            rPoints.RemoveAt(0);
+        }
+        if (Input.GetKeyDown("escape"))
+            Screen.lockCursor = false;
+        float rotationX = head.transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        head.transform.localEulerAngles = new Vector3(0, rotationX, 0);
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hitInfo;
+            Vector3 origin = transform.position + transform.right / 2 - transform.up / 3;
+            GameObject shot;
+            shot = (GameObject)Instantiate(rocket, origin, new Quaternion());
+            if (Physics.Raycast(transform.position, transform.forward, out hitInfo))
+            {
+                shot.transform.LookAt(hitInfo.point);
+                rTimes.Add(Time.time + Vector3.Distance(origin, hitInfo.point) / projSpeed);
+                rPoints.Add(hitInfo.point);
+            }
+            else //go to space
+            {
+                shot.transform.LookAt(origin + transform.forward);
+            }
+            Destroy(shot, 10f);
+        }
+    }
+	
+	void Start ()
+    {
+        marian = GameObject.Find("Marian");
+        Screen.lockCursor = true;
+        //Screen.showCursor = false;
+        head = GameObject.Find("Head");
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+	}
+}
---
+++ TheMightyMarian/Assets/Scripts/Move3D.cs
@@ -0,0 +1,199 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class Move3D : MonoBehaviour {
+    public AudioClip s_drop, s_jump;
+    Rigidbody rb;
+    public Vector3 input;
+    GameObject head;
+    public float speed = 6f;
+    public float currSpeed = 0f;
+    public float acc = 50f;
+    public float airCtrl = 0.1f;
+    public float friction = 20f;
+    public float jumpSpeed = 5f;
+    public float gravity = 15f;
+    public float circleJumpAcc = 8f;
+    public float bonusTime = 0.3f;
+    public float roundTime = 60f;
+    //public bool TrueForBuildFalseForEditor = true;
+    bool jumpPressed = false;
+    bool jumping = false;
+    float jumpPressTime = 0f;
+    float ctrl = 1f;
+    float enterTime = 0;
+    bool isTouchingGround = false;
+    float lastTrochZ = -3f;
+    float lastTrochY = 2f;
+    float mostZ = 0f;
+    GUIText tl;
+    GameObject[] torches;
+    float startTime = 0f;
+	// Use this for initialization
+	void Start () {
+        rb = transform.rigidbody;
+        head = GameObject.Find("Head");
+        torches = GameObject.FindGameObjectsWithTag("Torch");
+        tl = GameObject.Find("TimeLeft").guiText;
+        startTime = Time.time;
+        /*if (TrueForBuildFalseForEditor)
+            circleJumpAcc += 5f;*/
+	}
+
+    void Update()
+    {
+        tl.text = "Time left: " + (roundTime + startTime - Time.time);
+        if (startTime - Time.time > roundTime)
+            finish();
+        input = (Input.GetAxisRaw("Vertical") * head.transform.forward + Input.GetAxisRaw("Horizontal") * head.transform.right).normalized;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jumpPressed = true;
+            jumpPressTime = Time.time;
+        }
+        circleJumpAccelerate();
+        if (Input.GetKeyDown(KeyCode.KeypadPlus))
+        {
+            circleJumpAcc += 1f;
+        }
+        if (Input.GetKeyDown(KeyCode.KeypadMinus))
+        {
+            circleJumpAcc -= 1f;
+        }
+    }
+
+    private void finish()
+    {
+        float exp = -mostZ;
+        //load level and add exp;
+    }
+    void FixedUpdate()
+    {
+        if (transform.position.z < mostZ)
+            mostZ = transform.position.z;
+        if (rb.velocity.y > 0.1f)
+            jumping = true;
+        float mag;
+        Vector3 dir;
+        rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y - gravity * Time.deltaTime, rb.velocity.z); //GRAVITY
+        if (isTouchingGround)
+        {
+            foreach (GameObject t in torches) //checkpoint
+            {
+                if (transform.position.z < t.transform.position.z && t.transform.position.z < lastTrochZ)
+                {
+                    lastTrochZ = t.transform.position.z;
+                    lastTrochY = t.transform.position.y;
+                }
+            }
+            if (jumpPressed && Time.time - jumpPressTime < 0.1f) //jumping
+            {
+                jump();
+            }
+            else if (Time.time - enterTime > 0.1f && !jumping) //touching ground and aint gonna jump - apply friction
+            {
+                mag = new Vector2(rb.velocity.x, rb.velocity.z).magnitude;
+                dir = rb.velocity.normalized;
+                float multiply = 1f;
+                if (input.magnitude < 0.2f) //stronger friction if no input
+                    multiply = 2f;
+                float newMag = mag - friction * Time.deltaTime * multiply;
+                if (mag > 0)
+                    rb.velocity = new Vector3(dir.x * newMag, rb.velocity.y, dir.z * newMag);
+                else
+                    rb.velocity = new Vector3(0, rb.velocity.y, 0);
+                if (input.magnitude < 0.1f && rb.velocity.magnitude < 0.6f) //to stop the quake
+                    rb.velocity = new Vector3();
+            }
+            ctrl = 1f;
+        }
+        else
+            ctrl = airCtrl;
+        if(jumping)
+            ctrl = airCtrl;
+        dir = rb.velocity.normalized;
+        if (input.magnitude > 0.1f)
+        {
+            Vector2 newVel = new Vector2(rb.velocity.x + input.x * Time.deltaTime * acc * ctrl, rb.velocity.z + input.z * Time.deltaTime * acc * ctrl);
+            if (rb.velocity.magnitude < speed && newVel.magnitude > speed)
+                rb.velocity = new Vector3((newVel.normalized * speed).x, rb.velocity.y, (newVel.normalized * speed).y);
+            else if (rb.velocity.magnitude < speed)
+                rb.velocity = new Vector3(newVel.x, rb.velocity.y, newVel.y);
+            else
+            {
+                mag = new Vector2(rb.velocity.x, rb.velocity.z).magnitude;
+                rb.velocity = new Vector3(newVel.normalized.x * mag, rb.velocity.y, newVel.normalized.y * mag);
+            }
+        }
+        if (transform.position.y < -2) //reset
+        {
+            transform.position = new Vector3(9.5f, lastTrochY + bonusTime, lastTrochZ);
+            rb.velocity = new Vector3();
+        }
+
+        currSpeed = rb.velocity.magnitude;
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        jumping = false;
+        enterTime = Time.time;
+        isTouchingGround = true;
+        audio.PlayOneShot(s_drop);
+        Debug.Log("enter: " + enterTime);
+        if (jumpPressed && Time.time - jumpPressTime < 0.1f) //jump on touch;
+        {
+            jump();
+        }
+    }
+    void OnTriggerStay(Collider other)
+    {
+        isTouchingGround = true; 
+        if (rb.velocity.y < 0.1f)
+            jumping = false;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        isTouchingGround = false;
+    }
+    void jump()
+    {
+        jumping = true;
+        Debug.Log("jump: " + Time.time);
+        audio.PlayOneShot(s_jump);
+        Vector2 temp = new Vector2(rb.velocity.x, rb.velocity.z);
+        float mag2D = temp.magnitude;
+        Vector2 dir2D = temp.normalized;
+        float velY = jumpSpeed;
+        if (rb.velocity.y > 0)
+            velY += rb.velocity.y;
+        if (input.magnitude > 0.1f && temp.magnitude < speed && temp.magnitude > speed / 2)
+        {
+            temp = dir2D * speed;
+            rb.velocity = new Vector3(temp.x, velY, temp.y);
+        }
+        else
+        {
+            rb.velocity = new Vector3(dir2D.x * mag2D, velY, dir2D.y * mag2D);
+        }
+        jumpPressed = false;
+    }
+    void circleJumpAccelerate()
+    {
+        if (jumping && Time.time - jumpPressTime < 0.5f)
+        {
+            Vector2 temp = new Vector2(rb.velocity.x, rb.velocity.z);
+            float mag2D = temp.magnitude;
+            Vector2 dir2D = temp.normalized;
+            if (Input.GetAxisRaw("Horizontal") * Input.GetAxis("Mouse X") > 0.1f) //circle jump
+            {
+                mag2D += Time.deltaTime * circleJumpAcc;
+                rb.velocity = new Vector3(dir2D.x * mag2D, rb.velocity.y, dir2D.y * mag2D);
+            }
+            else if (Input.GetAxisRaw("Horizontal") * Input.GetAxis("Mouse X") < -0.1f) //wrong directions, slow down
+            {
+                mag2D -= Time.deltaTime * circleJumpAcc;
+                rb.velocity = new Vector3(dir2D.x * mag2D, rb.velocity.y, dir2D.y * mag2D);
+            }
+        }
+    }
+}
